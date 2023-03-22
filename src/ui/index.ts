7d68e222af2d0f2f6b44c6ead248fadb9da9377f
petrovskya <petrovska.ya@icloud.com>
2023-03-22 03:34:40 +0300

    feat(ui): add typography styles

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,6 @@
 import { GlobalStyles } from './GlobalStyles';
 import { Colors } from './colors';
 import { StyledOutlet } from './outletStyles';
+import { H1, H2, H3, S1, S2, S3, B1 } from './typography';
 
-export { GlobalStyles, Colors, StyledOutlet };
+export { GlobalStyles, Colors, StyledOutlet, H1, H2, H3, S1, S2, S3, B1 };
