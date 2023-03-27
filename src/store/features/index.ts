2eda327f023c91b7f0a3f3cacaacbdca46cab051
Yana Piatrouskaya <Yana.Piatrouskaya@itechart-group.com>
2023-03-27 16:27:12 +0300

    feat(store): update barrel pattern

--- src/store/features/index.ts
+++ src/store/features/index.ts
@@ -1,5 +1,15 @@
 import { fetchAllMovies } from './moviesSlice';
 import { fetchTrendsMovies } from './trendsSlice';
 import { fetchFullMovie } from './movieSlice';
+import { fetchNextMoviesPage } from './moviesSlice';
+import { changePage } from './moviesSlice';
+import { fetchNextTrendsPage } from './trendsSlice';
 
-export { fetchAllMovies, fetchTrendsMovies, fetchFullMovie };
+export {
+  fetchAllMovies,
+  fetchTrendsMovies,
+  fetchFullMovie,
+  changePage,
+  fetchNextMoviesPage,
+  fetchNextTrendsPage,
+};
